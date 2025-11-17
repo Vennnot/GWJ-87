c803354a7cf3fb59e275a533232082172e2f9965
Daniel Delgado <vennnot@gmail.com>
2025-11-17 21:25:41 +0100

    player movement

---
+++ scenes/player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+class_name Player
+
+@export var speed := 100.0
+@export var friction := 2000.0
+
+
+func _physics_process(delta:float)->void:
+	var input_vector := Input.get_vector("left", "right", "up", "down")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector * speed
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	move_and_slide()
