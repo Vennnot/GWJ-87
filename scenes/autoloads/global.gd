f8e8b13672397a21dff3749e9cb073e17faba1ea
dan-linux-laptop <vennnot@gmail.com>
2025-11-18 12:31:41 +0100

    idk

--- scenes/autoloads/global.gd
+++ scenes/autoloads/global.gd
@@ -1,3 +1,5 @@
 extends Node
 
+const PLAYER_SCENE : PackedScene = preload("res://scenes/player/player.tscn")
+
 var favor : float = 0
--- scenes/levels/level_template.gd
+++ scenes/levels/level_template.gd
@@ -1,8 +1,11 @@
 class_name LevelScene
 extends Node2D
 
+
 @export var next_level_info : LevelInfo
+
 @onready var level_transition_interactable: LevelTransitionInteractable = %LevelTransitionInteractable
+@onready var player_start_position: Marker2D = %PlayerStartPosition
 
 func _ready() -> void:
 	if not next_level_info:
@@ -11,7 +14,12 @@ func _ready() -> void:
 		return
 	
 	level_transition_interactable.interacted.connect(_on_interacted)
+	instantiate_player()
 
 
 func _on_interacted()->void:
 	SceneChanger.go_to_scene(next_level_info.next_scene_file, next_level_info.end_scene_text)
+
+
+func instantiate_player()->void:
+	pass
