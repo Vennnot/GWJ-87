2fa66fabca0d4550904c95dbdd18a978f28fc682
dan-linux-laptop <vennnot@gmail.com>
2025-11-19 15:30:49 +0100

    dialogue code

--- scenes/autoloads/global.gd
+++ scenes/autoloads/global.gd
@@ -3,3 +3,8 @@ extends Node
 const PLAYER_SCENE : PackedScene = preload("res://scenes/player/player.tscn")
 
 var favor : float = 0
+
+
+func change_favor(value:int)->void:
+	favor += value
+	print("Changed favor!")
